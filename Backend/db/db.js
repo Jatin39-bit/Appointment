a63eebfb30492ca4b9eb8897e9e13c81f8a1f4e5
Jatin39-bit <techrevain@gmail.com>
2025-02-13 17:17:04 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function connectDB() {
-    mongoose.connect(process.env.MONGO_URI || mongodb+srv://hoodajatin88:ZAQwsxzaqWSX12#@cluster0.dtjs0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0).then(() => { console.log('Connected to the database') }).catch((err) => { console.log(err) });
+    const fallbackURI = "mongodb+srv://hoodajatin88:ZAQwsxzaqWSX12%23@cluster0.dtjs0.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0";
+    const mongoURI = process.env.MONGO_URI || fallbackURI;
+    
+    mongoose.connect(mongoURI, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(() => { 
+        console.log('Connected to the database');
+    })
+    .catch((err) => { 
+        console.error('Database connection error:', err);
+    });
 }
 
 module.exports = connectDB;
