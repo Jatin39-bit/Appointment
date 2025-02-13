5fe3f6482adc0c206210139f481cef321d5ffdf3
Jatin39-bit <techrevain@gmail.com>
2025-02-13 17:09:01 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function connectDB() {
-    mongoose.connect(process.env.MONGO_URI).then(() => { console.log('Connected to the database') }).catch((err) => { console.log(err) });
+    mongoose.connect(process.env.MONGO_URI || mongodb+srv://hoodajatin88:ZAQwsxzaqWSX12#@cluster0.dtjs0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0).then(() => { console.log('Connected to the database') }).catch((err) => { console.log(err) });
 }
 
 module.exports = connectDB;
