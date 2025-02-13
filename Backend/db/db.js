d00b8821bc12a3b4a9aa93d78cb0516efc8668ae
Jatin39-bit <techrevain@gmail.com>
2025-02-13 17:51:55 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -6,7 +6,7 @@ function connectDB() {
     const fallbackURI = "mongodb+srv://hoodajatin88:ZAQwsxzaqWSX12%23@cluster0.dtjs0.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0";
     const mongoURI = process.env.MONGO_URI || fallbackURI;
     
-    mongoose.connect(mongoURI, { 
+    mongoose.connect(fallbackURI, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
     })
