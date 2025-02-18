a924ebb15b335f268e98e77bc405bbe5ab3239a5
Jatin39-bit <techrevain@gmail.com>
2025-02-18 20:40:53 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -29,6 +29,11 @@ module.exports.loginUser = async (req, res) => {
     }
     const { email, password } = req.body;
     const user = await userSerive.findUser(email);
+        if(!user || user.error){
+        return res.status(400).json({ errors: [{ msg: 'User not found' }] });
+    }else{
+        console.log(user);
+    }
     const isValidPassword = await user.isValidPassword(password);
     if (!isValidPassword) {
         return res.status(400).json({ errors: [{ msg: 'Invalid password' }] });
